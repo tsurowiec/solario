760e623b7142c5abf65bef94f2ad103a7ed8fc2b
Tomasz Surowiec <tsurowiec@gmail.com>
2026-05-24 20:07:25 +0200

    add deploy script on port 8001

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+git fetch origin
+git reset --hard origin/main
+composer install --no-dev --optimize-autoloader
+npm run build
+php artisan migrate --force
+php artisan optimize
+php artisan serve --host 0.0.0.0 --port=8001
